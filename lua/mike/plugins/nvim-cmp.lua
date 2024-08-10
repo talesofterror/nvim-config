3665169bfe3162ec85927047f59d21cb453f2dae
mike <mich.rivera84@gmail.com>
2024-08-09 21:31:00 -0400

    cmp bindings

--- lua/mike/plugins/nvim-cmp.lua
+++ lua/mike/plugins/nvim-cmp.lua
@@ -19,7 +19,7 @@ return {
 		-- load vs-code like snippets from plugins (e.g. friendly snippets) 
 		require("luasnip.loaders.from_vscode").lazy_load() 
 
-		vim.opt.completeopt = "menu,menuone,noselect"
+		vim.opt.completeopt = "menu,menuone"
 
 		cmp.setup({
 			snippet = {
@@ -28,8 +28,8 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(),
-				["C-j>"] = cmp.mapping.select_next_item(),
+				["<S-Tab>"] = cmp.mapping.select_prev_item(),
+				["<Tab>"] = cmp.mapping.select_next_item(),
 				["C-b"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space"] = cmp.mapping.complete(),
